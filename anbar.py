1cf7f8044c4a5ba6fb274b8004796e6bd026e78d
c1 <tp1050@gmail.com>
2021-10-11 15:35:12 +0330

    test

--- anbar.py
+++ anbar.py
@@ -4,21 +4,51 @@ import mysql.connector
 from mysql.connector import Error
 import sys
 
+class Javab(Object):
+    eCodery={
+        'eCode1':001
+        'ecode2':002
+        'ecode3':003
+        }
+    def Javab(self,j,e):
+        error=# -*- coding: utf-8 -*-
+        javab=j
+        
+    private javab=''
+    private error=''
+    def setError(e):
+        error =e
+    def setJavab(j):
+        javab=j
+    def getJavab():
+        return javab
+    def getError():
+        return Error
+
+
+
+
+
+
+
 #Block access to internal Variables
 defHost="192.168.5.17"
-user='doolsaz'
-password='22111357'
+defUser='doolsaz'
+defPassword='22111357'
 
 #Option for secure connection
 #Option for X-SQL sys
-
-def getAnbar():
+#ablity to connect via proxy
+#active mysql discovery
+def getAnbar(host=defHost,user=defUser,password=defPassword,secure=0):
+    if secure:
+        pass
     """ Connect to MySQL database """
     conn = None
     try:
-        conn = mysql.connector.connect( host="192.168.5.17",
-                                        user='doolsaz',
-                                        password='22111357',)
+        conn = mysql.connector.connect( host=defHost,
+                                        user=defUser,
+                                        password=defPassword):
         if conn.is_connected():
             print('Connected to MySQL database')
             # conn.escape_string()
@@ -29,8 +59,15 @@ def getAnbar():
 
 
 
+"""
+Execute any stmt
+#Verobse on off
+"""
+
 def exec(stmt):
-    print('%s'%stmt)
+    j=Javab()
+    #print('%s'%stmt)
+    #code will shit itself if conn is null
     conn = getAnbar()
     mc = conn.cursor(buffered=True)
     recs=""
@@ -41,12 +78,12 @@ def exec(stmt):
         print(recs)
     except Exception as e:
         print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
-        recs='Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e
+        j.error='Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e
             #print(agahi)
     mc.close()
     conn.close()
-    recs=str(recs)
-    return recs
+    j.javab=str(recs)
+    return j
 
 def begir():
     pass
