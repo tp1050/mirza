8a3f9d99ea932776bdb4f6e4f3c5c6d05fa3c307
c1 <tp1050@gmail.com>
2021-10-20 09:25:39 +0330

    Javab Ohject

--- Javab.py
+++ Javab.py
@@ -1,7 +1,32 @@
 class Javab(object):
-    def __init__(self, ret='', error='200'):
+    def __init__(self, ret='   ', error='200'):
         self.ret = ret
         self.error = error
+
+    def setError(e):
+        self.error =e
+
+    def setJavab(j):
+        self.ret=j
+
+    def getJavab():
+        return self.ret
+
+    def getError():
+        return self.error
+
+    def isMosbat():
+        if self.error=='100':
+            return True
+        return False
+
+    def save():
+        save(str())
+
+
+    def str(self):
+      tmp='{} \n {}'.format(self.ret,self.error)
+      return tmp
 
     # eCodery={
     #     '001': 'Access denied ',
@@ -9,22 +34,3 @@ class Javab(object):
     #     '003': 'No database selected',
     #     '100': 'success'
     #     }
-    def setError(e):
-        self.error =e
-    def setJavab(j):
-        self.ret=j
-    def getJavab():
-        return self.ret
-    def getError():
-        return self.error
-
-    def str(self):
-        tmp='{} \n {}'.format(self.ret,self.error)
-        return tmp
-    def isMosbat(self):
-        if self.error=='100':
-            return True
-        return False
-    def save(self):
-        save(str())
-
