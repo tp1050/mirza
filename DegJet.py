0588cc0283615ab82ae3b8672f2bdf5b567f4552
c1 <tp1050@gmail.com>
2021-11-10 05:32:34 +0330

    Latest of Mirza

---
+++ DegJet.py
@@ -0,0 +1,45 @@
+class DegJet(object):
+
+    def __init__(self,conn='No'):
+        self.test='test2'
+        self.conn=conn
+
+    def getDic(self):
+        """
+        REturns a dictionary containing the instance variables
+        """
+        return vars(self)
+
+    def begoo(self,sep=' '):
+        """
+        Produces a String CURRENT*STATE of the instance
+        """
+        ret = ''
+        dic=self.getDic()
+        for k in dic:
+            s = '{} : {} '.format(str(k) ,dic[k])+ sep + s
+        return s
+    def sabt(self):
+        dic=self.getDic()
+        s=''
+        for k in dic:
+            s=s+','+'{}'.format(k)
+        s=s[1:]
+        p=''
+        for k in dic:
+            p = p + ',' + '{}'.format(dic[k])
+        p = p[1:]
+        print(p)
+        # bezar()
+        # pp=berin(sign=',{}',n=len(dic.keys()))[1:]
+        # print(pp)
+        # i=0
+        # for k in dic:
+        #     berin(i,'+')
+        #     print(dic[k])
+        #     pp=pp.format()
+        #     print(pp)
+        #     i=i+1
+        # # stmt=self.begoo(sep=',')
+        # # bezar()
+        print(s)
